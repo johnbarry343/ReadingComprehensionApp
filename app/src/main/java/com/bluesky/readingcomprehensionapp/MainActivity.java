1161cc8f3f44f5d77aa66ac042ef588d86d94131
Tim <tthomasma.stevy@gmail.com>
2014-12-02 13:04:55 -0500

    MainActivity finished - need names of the four activities that students do, and images in "drawable" for the buttons.

--- app/src/main/java/com/bluesky/readingcomprehensionapp/MainActivity.java
+++ app/src/main/java/com/bluesky/readingcomprehensionapp/MainActivity.java
@@ -4,46 +4,65 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import bluesky.gc.com.readingcomprehensionapp.R;
 
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    Button
+    Button soundToText, soundToPicture, pictureToText, textToPictures;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        setContentView(R.layout.tim_activity_main);
+        soundToText = (Button) findViewById(R.id.soundToText);
+        soundToPicture = (Button) findViewById(R.id.soundToPicture);
+        pictureToText = (Button) findViewById(R.id.pictureToText);
+        textToPictures = (Button) findViewById(R.id.textToPictures);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
+    public void onClick(View v) {
+        boolean goodProblem = true;
+        int vid = v.getId();
+        switch (vid) {
+            case R.id.soundToText: {
+//put name of sound-to-text activity in place of "ResultActivity"
+                //Intent intent = new Intent(this, ResultActivity.class);
+                //intent.putExtra("gameState", gameState);
+                //startActivity(intent);
+                //finish();
+                break;
+            }
+            case R.id.soundToPicture: {
+//put name of sound-to-picture activity in place of "ResultActivity"
+                //Intent intent = new Intent(this, ResultActivity.class);
+                //intent.putExtra("gameState", gameState);
+                //startActivity(intent);
+                //finish();
+                break;
+            }
+            case R.id.pictureToText: {
+//put name of picture-to-text activity in place of "ResultActivity"
+                //Intent intent = new Intent(this, ResultActivity.class);
+                //intent.putExtra("gameState", gameState);
+                //startActivity(intent);
+                //finish();
+                break;
+            }
+            case R.id.textToPictures: {
+//put name of text-to-pictures activity in place of "ResultActivity"
+                //Intent intent = new Intent(this, ResultActivity.class);
+                //intent.putExtra("gameState", gameState);
+                //startActivity(intent);
+                //finish();
+                break;
+            }
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
 }
