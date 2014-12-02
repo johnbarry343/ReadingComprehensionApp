99180f32c96b986f78813f1a00f4b4746911a542
Tim <tthomasma.stevy@gmail.com>
2014-12-02 16:59:43 -0500

    Put code in SoundToPictureActivity to capture button clicks

--- app/src/main/java/com/bluesky/readingcomprehensionapp/SoundToPictureActivity.java
+++ app/src/main/java/com/bluesky/readingcomprehensionapp/SoundToPictureActivity.java
@@ -3,6 +3,7 @@ package com.bluesky.readingcomprehensionapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.bluesky.readingcomprehensionapp.R;
 
@@ -11,18 +12,45 @@ import com.bluesky.readingcomprehensionapp.R;
  */
 public class SoundToPictureActivity extends Activity implements View.OnClickListener {
     //Tim is doing this one
+    ImageButton imageButton5, imageButton6, imageButton8, imageButton9;
+    int correctAnswer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sound_to_pic);
-
-
+        imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(this);
+        imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        imageButton6.setOnClickListener(this);
+        imageButton8 = (ImageButton) findViewById(R.id.imageButton8);
+        imageButton8.setOnClickListener(this);
+        imageButton9 = (ImageButton) findViewById(R.id.imageButton9);
+        imageButton9.setOnClickListener(this);
+        //draw new problem. Need to store the button ID of the correct answer in correctAnswer
     }
 
     @Override
-    public void onClick(View arg0) {
-
-
+    public void onClick(View v) {
+        boolean gotItRight = false;
+        int vid = v.getId();
+        switch (vid) {
+            case R.id.imageButton5: {
+                if (R.id.imageButton5 == correctAnswer) gotItRight = true;
+                break;
+            }
+            case R.id.imageButton6: {
+                if (R.id.imageButton6 == correctAnswer) gotItRight = true;
+                break;
+            }
+            case R.id.imageButton8: {
+                if (R.id.imageButton8 == correctAnswer) gotItRight = true;
+                break;
+            }
+            case R.id.imageButton9: {
+                if (R.id.imageButton9 == correctAnswer) gotItRight = true;
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/bluesky/readingcomprehensionapp/SoundToTextActivity.java
+++ app/src/main/java/com/bluesky/readingcomprehensionapp/SoundToTextActivity.java
@@ -54,130 +54,218 @@ public class SoundToTextActivity extends Activity {
     private int triangle;
 
 
-
     boolean plays = false, loaded = false;
     float actVolume, maxVolume, volume;
     AudioManager audioManager;
 
-    private void ball(){ if (loaded && !plays){ soundPool.play(ball,volume, volume, 1, 0, 1f);
-        plays = false;
+    private void ball() {
+        if (loaded && !plays) {
+            soundPool.play(ball, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void bear() {
+        if (loaded && !plays) {
+            soundPool.play(bear, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void bear(){ if (loaded && !plays){ soundPool.play(bear,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void bee() {
+        if (loaded && !plays) {
+            soundPool.play(bee, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void book() {
+        if (loaded && !plays) {
+            soundPool.play(book, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void bee(){ if (loaded && !plays){ soundPool.play(bee,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void bus() {
+        if (loaded && !plays) {
+            soundPool.play(bus, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void car() {
+        if (loaded && !plays) {
+            soundPool.play(car, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void book(){ if (loaded && !plays){ soundPool.play(book,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void cat() {
+        if (loaded && !plays) {
+            soundPool.play(cat, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void circle() {
+        if (loaded && !plays) {
+            soundPool.play(circle, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void bus(){ if (loaded && !plays){ soundPool.play(bus,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void cow() {
+        if (loaded && !plays) {
+            soundPool.play(cow, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void dog() {
+        if (loaded && !plays) {
+            soundPool.play(dog, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void car(){ if (loaded && !plays){ soundPool.play(car,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void door() {
+        if (loaded && !plays) {
+            soundPool.play(door, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void duck() {
+        if (loaded && !plays) {
+            soundPool.play(duck, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void cat(){ if (loaded && !plays){ soundPool.play(cat,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void egg() {
+        if (loaded && !plays) {
+            soundPool.play(egg, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void fish() {
+        if (loaded && !plays) {
+            soundPool.play(fish, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void circle(){ if (loaded && !plays){ soundPool.play(circle,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void flower() {
+        if (loaded && !plays) {
+            soundPool.play(flower, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void frog() {
+        if (loaded && !plays) {
+            soundPool.play(frog, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void cow(){ if (loaded && !plays){ soundPool.play(cow,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void goat() {
+        if (loaded && !plays) {
+            soundPool.play(goat, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void hat() {
+        if (loaded && !plays) {
+            soundPool.play(hat, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void dog(){ if (loaded && !plays){ soundPool.play(dog,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void house() {
+        if (loaded && !plays) {
+            soundPool.play(house, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void leaf() {
+        if (loaded && !plays) {
+            soundPool.play(leaf, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void door(){ if (loaded && !plays){ soundPool.play(door,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void lion() {
+        if (loaded && !plays) {
+            soundPool.play(lion, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void moon() {
+        if (loaded && !plays) {
+            soundPool.play(moon, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void duck(){ if (loaded && !plays){ soundPool.play(duck,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void pie() {
+        if (loaded && !plays) {
+            soundPool.play(pie, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void ship() {
+        if (loaded && !plays) {
+            soundPool.play(ship, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void egg(){ if (loaded && !plays){ soundPool.play(egg,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void sky() {
+        if (loaded && !plays) {
+            soundPool.play(sky, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void star() {
+        if (loaded && !plays) {
+            soundPool.play(star, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void fish(){ if (loaded && !plays){ soundPool.play(fish,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void sun() {
+        if (loaded && !plays) {
+            soundPool.play(sun, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
+
+    private void square() {
+        if (loaded && !plays) {
+            soundPool.play(square, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    private void flower(){ if (loaded && !plays){ soundPool.play(flower,volume, volume, 1, 0, 1f);
-        plays = false;
+
+    private void tree() {
+        if (loaded && !plays) {
+            soundPool.play(tree, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
-    }
-    private void frog(){ if (loaded && !plays){ soundPool.play(frog,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void goat(){ if (loaded && !plays){ soundPool.play(goat,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void hat(){ if (loaded && !plays){ soundPool.play(hat,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void house(){ if (loaded && !plays){ soundPool.play(house,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void leaf(){ if (loaded && !plays){ soundPool.play(leaf,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void lion(){ if (loaded && !plays){ soundPool.play(lion,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void moon(){ if (loaded && !plays){ soundPool.play(moon,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void pie(){ if (loaded && !plays){ soundPool.play(pie,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void ship(){ if (loaded && !plays){ soundPool.play(ship,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void sky(){ if (loaded && !plays){ soundPool.play(sky,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void star(){ if (loaded && !plays){ soundPool.play(star,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void sun(){ if (loaded && !plays){ soundPool.play(sun,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void square(){ if (loaded && !plays){ soundPool.play(square,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void tree(){ if (loaded && !plays){ soundPool.play(tree,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
-    }
-    private void triangle(){ if (loaded && !plays){ soundPool.play(triangle,volume, volume, 1, 0, 1f);
-        plays = false;
-    }
+
+    private void triangle() {
+        if (loaded && !plays) {
+            soundPool.play(triangle, volume, volume, 1, 0, 1f);
+            plays = false;
+        }
     }
 
 
@@ -208,52 +296,46 @@ public class SoundToTextActivity extends Activity {
         });
 
 
+        ball = soundPool.load(this, R.raw.ball, 1);
+        bear = soundPool.load(this, R.raw.bear, 1);
+        bee = soundPool.load(this, R.raw.bee, 1);
+        book = soundPool.load(this, R.raw.book, 1);
+        bus = soundPool.load(this, R.raw.bus, 1);
+        car = soundPool.load(this, R.raw.car, 1);
+        cat = soundPool.load(this, R.raw.cat, 1);
+        circle = soundPool.load(this, R.raw.circle, 1);
+        cow = soundPool.load(this, R.raw.cow, 1);
+        dog = soundPool.load(this, R.raw.dog, 1);
+        door = soundPool.load(this, R.raw.door, 1);
+        duck = soundPool.load(this, R.raw.duck, 1);
+        egg = soundPool.load(this, R.raw.egg, 1);
+        fish = soundPool.load(this, R.raw.fish, 1);
+        flower = soundPool.load(this, R.raw.flower, 1);
+        frog = soundPool.load(this, R.raw.frog, 1);
+        goat = soundPool.load(this, R.raw.goat, 1);
+        hat = soundPool.load(this, R.raw.hat, 1);
+        house = soundPool.load(this, R.raw.house, 1);
+        leaf = soundPool.load(this, R.raw.leaf, 1);
+        lion = soundPool.load(this, R.raw.lion, 1);
+        moon = soundPool.load(this, R.raw.moon, 1);
+        pie = soundPool.load(this, R.raw.pie, 1);
+        ship = soundPool.load(this, R.raw.ship, 1);
+        sky = soundPool.load(this, R.raw.sky, 1);
+        star = soundPool.load(this, R.raw.star, 1);
+        sun = soundPool.load(this, R.raw.sun, 1);
+        square = soundPool.load(this, R.raw.square, 1);
+        tree = soundPool.load(this, R.raw.tree, 1);
+        triangle = soundPool.load(this, R.raw.triangle, 1);
 
 
+    }
 
-    ball=soundPool.load(this,R.raw.ball,1);
-    bear=soundPool.load(this,R.raw.bear,1);
-    bee=soundPool.load(this,R.raw.bee,1);
-    book=soundPool.load(this,R.raw.book,1);
-    bus=soundPool.load(this,R.raw.bus,1);
-    car=soundPool.load(this,R.raw.car,1);
-    cat=soundPool.load(this,R.raw.cat,1);
-    circle=soundPool.load(this,R.raw.circle,1);
-    cow=soundPool.load(this,R.raw.cow,1);
-    dog=soundPool.load(this,R.raw.dog,1);
-    door=soundPool.load(this,R.raw.door,1);
-    duck=soundPool.load(this,R.raw.duck,1);
-    egg=soundPool.load(this,R.raw.egg,1);
-    fish=soundPool.load(this,R.raw.fish,1);
-    flower=soundPool.load(this,R.raw.flower,1);
-    frog=soundPool.load(this,R.raw.frog,1);
-    goat=soundPool.load(this,R.raw.goat,1);
-    hat=soundPool.load(this,R.raw.hat,1);
-    house=soundPool.load(this,R.raw.house,1);
-    leaf=soundPool.load(this,R.raw.leaf,1);
-    lion=soundPool.load(this,R.raw.lion,1);
-    moon=soundPool.load(this,R.raw.moon,1);
-    pie=soundPool.load(this,R.raw.pie,1);
-    ship=soundPool.load(this,R.raw.ship,1);
-    sky=soundPool.load(this,R.raw.sky,1);
-    star=soundPool.load(this,R.raw.star,1);
-    sun=soundPool.load(this,R.raw.sun,1);
-    square=soundPool.load(this,R.raw.square,1);
-    tree=soundPool.load(this,R.raw.tree,1);
-    triangle=soundPool.load(this,R.raw.triangle,1);
-
-
-
-}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sound_to_word);
-
-
-
 
 
     }
