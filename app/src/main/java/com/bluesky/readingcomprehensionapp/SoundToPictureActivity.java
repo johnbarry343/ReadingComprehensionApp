81b8290e687db2ab8c49c233146e23f7cdbc6897
Tim <tthomasma.stevy@gmail.com>
2014-12-03 16:57:08 -0500

    SoundToPictureActivity uses method in ActivityUtilities to open right-answer AlertDialog.

--- app/src/main/java/com/bluesky/readingcomprehensionapp/SoundToPictureActivity.java
+++ app/src/main/java/com/bluesky/readingcomprehensionapp/SoundToPictureActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.bluesky.readingcomprehensionapp.R;
 
@@ -55,6 +56,11 @@ public class SoundToPictureActivity extends Activity implements View.OnClickList
                 break;
             }
         }
+        gotItRight = true;
         if ( !gotItRight ) ActivityUtilities.wrongAnswerToast(this, inflater);
+        ActivityUtilities.rightAnswerAlertDialog(this);
+        Toast.makeText(this,
+                "New question.",
+                Toast.LENGTH_LONG).show();
     }
 }
